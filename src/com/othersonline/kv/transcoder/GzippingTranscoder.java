805eba7db030af692cff31cb35c99144a032ec45
Sam Tingleff <sam@sam-desktop.(none)>
2009-04-27 09:37:27 -0700

    small optimization to buffer size in gzipping transcoder

--- src/com/othersonline/kv/transcoder/GzippingTranscoder.java
+++ src/com/othersonline/kv/transcoder/GzippingTranscoder.java
@@ -3,7 +3,6 @@ package com.othersonline.kv.transcoder;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -23,7 +22,7 @@ public class GzippingTranscoder implements Transcoder {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ByteArrayInputStream in = new ByteArrayInputStream(bytes);
 		GZIPInputStream gunzip = new GZIPInputStream(in);
-		byte[] buffer = new byte[4 * 1024];
+		byte[] buffer = new byte[bytes.length];
 		int read = 0;
 		while ((read = gunzip.read(buffer)) > 0) {
 			baos.write(buffer, 0, read);
