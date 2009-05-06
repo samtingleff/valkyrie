194d0e32c8438c8a3c6ab06a27e9fb39923347b7
Sam Tingleff <sam@sam-desktop.(none)>
2009-05-06 14:00:01 -0700

    a delegating spy memcached transcoder

---
+++ src/com/othersonline/kv/transcoder/spy/SpyMemcachedDelegatingTranscoder.java
@@ -0,0 +1,34 @@
+package com.othersonline.kv.transcoder.spy;
+
+import java.io.IOException;
+
+import com.othersonline.kv.transcoder.SerializingTranscoder;
+
+import net.spy.memcached.CachedData;
+import net.spy.memcached.transcoders.Transcoder;
+
+public class SpyMemcachedDelegatingTranscoder<T> implements Transcoder<T> {
+
+	private com.othersonline.kv.transcoder.Transcoder delegate = new SerializingTranscoder();
+
+	public T decode(CachedData data) {
+		if (data == null)
+			return null;
+		byte[] bytes = data.getData();
+		try {
+			return ((T) delegate.decode(bytes));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public CachedData encode(T object) {
+		try {
+			byte[] bytes = delegate.encode(object);
+			return new CachedData(0, bytes);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
