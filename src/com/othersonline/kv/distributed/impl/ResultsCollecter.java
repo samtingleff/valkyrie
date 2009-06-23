cf305bbb14c716cec3a5625189e47e9d93cdbde6
stingleff <stingleff@Sam-Tingleffs-MacBook-Pro.local>
2009-06-23 15:39:40 -0700

    fix for very unfrequent ConcurrentModificationException on get()

--- src/com/othersonline/kv/distributed/impl/ResultsCollecter.java
+++ src/com/othersonline/kv/distributed/impl/ResultsCollecter.java
@@ -1,11 +1,13 @@
 package com.othersonline.kv.distributed.impl;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
 
 public class ResultsCollecter<V> implements Iterable<V> {
 	private List<V> results;
+
 	private boolean stopped = false;
 
 	public ResultsCollecter(int size) {
@@ -25,7 +27,40 @@ public class ResultsCollecter<V> implements Iterable<V> {
 		this.stopped = true;
 	}
 
+	/*
+	 * Attempt at an iterator that will not throw
+	 * ConcurrentModificationException when new results are appended to the
+	 * list.
+	 */
 	public Iterator<V> iterator() {
-		return results.iterator();
+		return new Iterator<V>() {
+			private int index = 0;
+
+			private List<V> delegate;
+
+			public Iterator<V> init(List<V> delegate) {
+				this.delegate = delegate;
+				return this;
+			}
+
+			public boolean hasNext() {
+				return (index < delegate.size());
+			}
+
+			public V next() {
+				V next = null;
+				try {
+					next = delegate.get(index);
+				} catch (IndexOutOfBoundsException e) {
+					throw new ConcurrentModificationException(e.getMessage());
+				} finally {
+					++index;
+				}
+				return next;
+			}
+
+			public void remove() {
+			}
+		}.init(this.results);
 	}
 }
