840bb5970a41ea6f404c9cf093da9040e618aa87
stingleff <stingleff@Sam-Tingleffs-MacBook-Pro.local>
2009-08-17 13:22:48 -0700

    formatting

--- src/com/othersonline/kv/backends/TokyoTyrantKeyValueStore.java
+++ src/com/othersonline/kv/backends/TokyoTyrantKeyValueStore.java
@@ -441,8 +441,8 @@ public class TokyoTyrantKeyValueStore extends BaseManagedKeyValueStore
 		} finally {
 			releaseRDB(rdb);
 		}
-		
 	}
+
 	public Object[] fwmkeys(String prefix, int max)
 			throws KeyValueStoreException {
 		assertReadable();
