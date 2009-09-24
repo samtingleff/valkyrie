0d9129227c2ab168d7d8cd60c63d72382b966c0c
stingleff <stingleff@mlo-bz560.rubicorp.com>
2009-09-23 17:11:29 -0700

    more instrumentation, print intermediate results

--- src/com/othersonline/kv/tools/ValkyrieRebalance.java
+++ src/com/othersonline/kv/tools/ValkyrieRebalance.java
@@ -17,6 +17,7 @@ import org.kohsuke.args4j.Option;
 import com.othersonline.kv.KeyValueStore;
 import com.othersonline.kv.KeyValueStoreUnavailable;
 import com.othersonline.kv.backends.IterableKeyValueStore;
+import com.othersonline.kv.backends.KeyValueStoreIterator;
 import com.othersonline.kv.backends.UriConnectionFactory;
 import com.othersonline.kv.distributed.Node;
 import com.othersonline.kv.distributed.impl.DistributedKeyValueStoreClientImpl;
@@ -58,17 +59,20 @@ import com.othersonline.kv.transcoder.Transcoder;
  */
 public class ValkyrieRebalance implements Runnable, Callable<Map<String, Long>> {
 
-	@Option(name = "--source", usage = "Source uri")
+	@Option(name = "--source", usage = "Source uri (default: none)", required = true)
 	private String source;
 
-	@Option(name = "--node", usage = "Source node id")
+	@Option(name = "--node", usage = "Source node id (default: none)", required = true)
 	private int nodeId;
 
-	@Option(name = "--sleep", usage = "Sleep time between keys (millis)")
+	@Option(name = "--properties", usage = "Properties file for destination valkyrie client (default: none)", required = true)
+	private String properties;
+
+	@Option(name = "--sleep", usage = "Sleep time between keys (millis) (default: disabled)")
 	private long sleep = 0;
 
-	@Option(name = "--properties", usage = "Properties file for destination valkyrie client")
-	private String properties;
+	@Option(name = "--max", usage = "Stop after x keys (default: disabled)")
+	private int max = -1;
 
 	private Transcoder byteTranscoder = new ByteArrayTranscoder();
 
@@ -97,56 +101,74 @@ public class ValkyrieRebalance implements Runnable, Callable<Map<String, Long>> 
 		IterableKeyValueStore src = getSource();
 		DistributedKeyValueStoreClientImpl valkyrie = getDestination();
 
-		long moved = 0, unmoved = 0, getFailures = 0, setFailures = 0, deleteFailures = 0;
+		long examined = 0, moved = 0, notMoved = 0, getFailures = 0, setFailures = 0, deleteFailures = 0;
 		int writeReplicas = Integer.parseInt(props
 				.getProperty("write.replicas"));
-		Iterator<String> iter = src.iterkeys().iterator();
-		while (iter.hasNext()) {
-			String key = iter.next();
-			List<Node> preferenceList = valkyrie.getPreferenceList(key,
-					writeReplicas);
-			boolean set = true;
-			for (Node node : preferenceList) {
-				if (node.getId() == nodeId) {
-					set = false;
-					break;
-				}
-			}
-			if (set) {
-				byte[] bytes = null;
-				boolean successfulMove = false;
-				try {
-					bytes = (byte[]) src.get(key, byteTranscoder);
-				} catch (Exception e) {
-					e.printStackTrace();
-					++getFailures;
-				}
-				try {
-					if (bytes != null) {
-						valkyrie.set(key, bytes, byteTranscoder);
-						successfulMove = true;
-						++moved;
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-					++setFailures;
-				}
-				try {
-					if (successfulMove)
-						src.delete(key);
-				} catch (Exception e) {
-					e.printStackTrace();
-					++deleteFailures;
-				}
-			} else
-				++unmoved;
+		KeyValueStoreIterator keyIterator = src.iterkeys();
+		try {
+			Iterator<String> iter = keyIterator.iterator();
+			while (iter.hasNext()) {
+				String key = iter.next();
+				++examined;
 
-			if (sleep > 0)
-				Thread.sleep(sleep);
+				List<Node> preferenceList = valkyrie.getPreferenceList(key,
+						writeReplicas);
+				boolean set = true;
+				for (Node node : preferenceList) {
+					if (node.getId() == nodeId) {
+						set = false;
+						break;
+					}
+				}
+				if (set) {
+					byte[] bytes = null;
+					boolean successfulMove = false;
+					try {
+						bytes = (byte[]) src.get(key, byteTranscoder);
+					} catch (Exception e) {
+						e.printStackTrace();
+						++getFailures;
+					}
+					try {
+						if (bytes != null) {
+							valkyrie.set(key, bytes, byteTranscoder);
+							successfulMove = true;
+							++moved;
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						++setFailures;
+					}
+					try {
+						if (successfulMove)
+							src.delete(key);
+					} catch (Exception e) {
+						e.printStackTrace();
+						++deleteFailures;
+					}
+				} else
+					++notMoved;
+
+				if (examined % 1000 == 0) {
+					System.out.println("examined: " + examined);
+					System.out.println("moved: " + moved);
+					System.out.println("getFailures: " + getFailures);
+					System.out.println("setFailures: " + setFailures);
+					System.out.println("deleteFailures: " + deleteFailures);
+				}
+				if ((max > 0) && (examined >= max))
+					break;
+
+				if (sleep > 0)
+					Thread.sleep(sleep);
+			}
+		} finally {
+			keyIterator.close();
 		}
 		Map<String, Long> results = new HashMap<String, Long>();
+		results.put("examined", examined);
 		results.put("moved", moved);
-		results.put("unmoved", unmoved);
+		results.put("not-moved", notMoved);
 		results.put("get-failures", getFailures);
 		results.put("set-failures", setFailures);
 		results.put("delete-failures", deleteFailures);
